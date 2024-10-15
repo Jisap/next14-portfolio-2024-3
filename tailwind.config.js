a920942d5e1caae322d1d782aed4f00dbd293b49
Jorge Matos <jisapjisap@gmail.com>
2024-10-15 13:41:54 +0200

    init

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,10 +6,26 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    container:{
+      center: true,
+      padding: "15px",
+    },
+    screens:{
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+    },
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        colors: {
+          primary: "#10151f",
+          secondary: "#475467",
+          accent: {
+            DEFAULT: "#7edad2",
+            hover: "#79d3cc",
+          }
+        }
       },
     },
   },
