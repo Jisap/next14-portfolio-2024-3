5789c502a46e4af2852cb73e5162da43a671fe6c
Jorge Matos <jisapjisap@gmail.com>
2024-10-16 13:04:33 +0200

    Hero

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+"use client"
+
+import Image from "next/image"
+import { Link as ScrollLink } from "react-scroll"
+import { TypeAnimation } from "react-type-animation"
+
+
 
 const Hero = () => {
   return (
-    <section>
-      Hero
+    <section className="h-[800px] relative bg-accent/10 xl:bg-white">
+      {/* header */}
+      <div>
+        header
+      </div>
+      <div className="container mx-auto h-full">
+        <div className="relative z-20 h-full w-full xl:max-w-[768px] flex flex-col items-center xl:items-start justify-center text-center xl:text-left pt-10">
+          <h1 className="h1">
+            <span className="text-accent">I Build And</span> Design Powerful
+            <TypeAnimation 
+              preRenderFirstString={true}
+              sequence={[
+                 "Websites", 2000,
+                 "Apps", 2000,
+              ]}
+              speed={50}
+              repeat={Infinity}
+              wrapper="span"
+              cursor={false}
+              className="ml-2 xl:ml-4"
+            />
+          </h1>
+        </div>
+      </div>
     </section>
   )
 }
