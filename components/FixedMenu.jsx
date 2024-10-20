4b40302500f2ea368f9ccee484882a97674857e2
Jorge Matos <jisapjisap@gmail.com>
2024-10-20 21:04:44 +0200

    FixedMenu

--- components/FixedMenu.jsx
+++ components/FixedMenu.jsx
@@ -1,6 +1,6 @@
 import { AnimatePresence, motion } from "framer-motion"
 import { useEffect, useState } from "react"
-import { CgMediaQuery } from "react-icons/cg"
+import { CgMediaQuery, CgMenu, CgMenuGridR } from "react-icons/cg"
 import { useMediaQuery } from "react-responsive"
 import  { FiMapPin, FiPhoneCall, FiMail } from "react-icons/fi"
 import Nav from "./Nav"
@@ -10,7 +10,7 @@ const FixedMenu = () => {
 
   const [showMenuButton, setShowMenuButton] = useState(false);
   const [showMenu, setShowMenu] = useState(false);
-  count [isMounted, setIsMounted] = useState(false);
+  const [isMounted, setIsMounted] = useState(false);
 
   const isMobile = useMediaQuery({ 
     query: "(max-width: 640px)" 
@@ -32,14 +32,45 @@ const FixedMenu = () => {
         return () => window.removeEventListener("scroll", handleScroll); // Remove the scroll event listener when the component is unmounted
       }else{
         setShowMenuButton(true);                                         // Show the button on mobile devices
-
       }
     }
   },[isMobile, isMounted]);
 
+  if(!isMounted) return null;                                            // prevent rendering until mounted
+
   return (
-    <div>
-      FixedMenu
+    <div className="fixed w-full h-[400px] z-50 flex justify-center pointer-events-none bg-transparent">
+      {/* menu */}
+      <div>Menu</div>
+      {/* menu button */}
+      {/* render button without animation on mobile devices */}
+      {isMobile ? (
+        <div>
+          <button>
+            <CgMenuGridR />
+          </button>
+        </div>
+      ) : (
+        <AnimatePresence>
+          {showMenuButton && (
+            <motion.div
+              initial={{ y: 100, opacity: 0}}
+              animate={{ y: 0, opacity: 1}}
+              exit= {{ y: 100, opacity: 0}}
+              transition={{ 
+                type: "spring",
+                stiffness: 400,
+                damping: 40
+              }}
+              className="fixed z-50 bottom-16 pointer-events-auto"
+            >
+              <button className="bg-accent shadow-custom w-[54px] h-[54px] rounded-lg cursor-pointer flex items-center justify-center select-none">
+                C<CgMenuGridR className="text-4xl text-white" />
+              </button>
+            </motion.div>
+          )}
+        </AnimatePresence>
+      )}
     </div>
   )
 }
