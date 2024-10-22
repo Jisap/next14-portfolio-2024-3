a6cf2affc8886defac962ee7176be844ede31b4b
Jorge Matos <jisapjisap@gmail.com>
2024-10-22 19:47:37 +0200

    about

--- components/About.jsx
+++ components/About.jsx
@@ -3,14 +3,35 @@ import Image from "next/image"
 import AnimatedText from "./AnimatedText"
 
 
+      // <AnimatedText 
+      //   text="This is a title" 
+      //   textStyles="h2"
+      // />
 
 const About = () => {
   return (
-    <div className='bg-yellow-200 py-96' id="about">
-      <AnimatedText 
-        text="This is a title" 
-        textStyles="h2"
-      />
+    <div className='relative pt-12 pb-24' id="about">
+      <div className="container mx-auto h-full">
+        <div className="h-full flex items-center justify-center">
+          <div className="hidden xl:flex flex-1 pl-8">
+            <div className="relative w-full max-w-[380px]">
+              {/* shape */}
+              <div className="w-[160px] h-[160px] bg-accent absolute -left-5 -top-5 -z-10"></div>
+              {/* image */}
+              <div className="rounded-tl-[8px] rounded-tr-[120px] w-full bg-[#e5f8f6] min-h-[480px] flex items-end justify-center">
+                <Image 
+                  src="/assets/about/img.png"
+                  width={350}
+                  height={478}
+                  quality={100}
+                  priority
+                  alt="aboutImg"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
