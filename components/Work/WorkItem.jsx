9b05ac80d94fa7f42787ae66299f586c6691c8d4
Jorge Matos <jisapjisap@gmail.com>
2024-10-25 16:43:47 +0200

    workItem

--- components/Work/WorkItem.jsx
+++ components/Work/WorkItem.jsx
@@ -1,8 +1,20 @@
+import Image from "next/image";
+import Link from "next/link";
+import { FiArrowRight } from "react-icons/fi";
+import { Badge } from "@/components/ui/badge"
 
 
-const WorkItem = () => {
+
+const WorkItem = ({href, category, img, title}) => {
   return (
-    <div>WorkItem</div>
+    <Link 
+      href="/"
+      className="group"
+    >
+      <div className="w-full h-[300px] p-8 rounded-[30px] flex items-center justify-center mb-6 relative overflow-hidden bg-[#f4f4f4]">
+        <Badge>{category}</Badge>
+      </div>
+    </Link>
   )
 }
 
