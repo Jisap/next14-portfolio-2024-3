d6d78a055f73f38c984907f35f574f25a4fe1fdb
Jorge Matos <jisapjisap@gmail.com>
2024-10-28 15:52:06 +0100

    submit

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -58,7 +58,10 @@ const Contact = () => {
               textStyles={`h2 mb-12 xl:text-left`}
             />
             {/* form */}
-            <form>
+            <form 
+              onSubmit={handleFormSubmit}
+              className="flex flex-col gap-6 w-full max-w-[480px]"
+            >
               {/* first name & lastname fields */}
               <div className="flex gap-8">
                 <div className="flex-1">
@@ -92,6 +95,93 @@ const Contact = () => {
                   />
                 </div>
               </div>
+              {/* email field*/}
+              <div>
+                <label htmlFor="email" className="block mb-2 text-sm font-medium text-primary">
+                  Email <span className="text-accent">*</span>
+                </label>
+                <input
+                  type="text"
+                  id="email"
+                  name="email"
+                  value={formData.email}
+                  onChange={handleChange}
+                  className="input"
+                  placeholder="youremail@example.com"
+                  required
+                />
+                <ValidationError 
+                  prefix="Email"
+                  field="email"
+                  errors={state.errors}  
+                />
+              </div>
+              {/* phone field*/}
+              <div>
+                <label htmlFor="phone" className="block mb-2 text-sm font-medium text-primary">
+                  Phone number<span className="text-accent">*</span>
+                </label>
+                <input
+                  type="text"
+                  id="phone"
+                  name="phone"
+                  value={formData.phone}
+                  onChange={handleChange}
+                  className="input"
+                  placeholder="123 456 789"
+                />
+              </div>
+              {/* message field*/}
+              <div>
+                <label htmlFor="message" className="block mb-2 text-sm font-medium text-primary">
+                  Message <span className="text-accent">*</span>
+                </label>
+                <textarea
+                  id="message"
+                  name="message"
+                  value={formData.message}
+                  onChange={handleChange}
+                  className="textarea"
+                  placeholder="Leave me a message..."
+                  rows="5"
+                  required
+                />
+                <ValidationError
+                  prefix="Message"
+                  field="message"
+                  errors={state.errors}
+                />
+              </div>
+              {/* Submit button */}
+              <button 
+                type="submit"
+                disabled={state.submitting}
+                className="btn btn-accent flex items-center justify-center gap-2"
+              >
+                {
+                  state.submitting 
+                  ? (<span>Sending...</span>)
+                  : (
+                    <>
+                      {/* show icon with opacity transition*/}
+                      <FaCheckCircle 
+                        className={`
+                          absolute text-white text-lg transition-opacity duration-500 ease-in-out 
+                          ${showIcon ? "opacity-100" : "opacity-0"}`
+                        }
+                      />
+                      <span 
+                        className={
+                          `transition-opacity duration-500 ease-linear 
+                          ${showIcon ? "opacity-0" : "opacity-100"}`
+                        }
+                      >
+                        Send message
+                      </span>
+                    </>
+                  )
+                }
+              </button>
             </form>
           </div>
         </div>
